cf0042af36450af17a64cd7ad49f3f52d08a3f68
Justin Hamerly <suvimountain@icloud.com>
2022-12-16 21:50:09 -0800

    converted hub server to class and implemented pet service queue subscriptions

--- pet-service/pet-service.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const Chance = require('chance');
-const chance = new Chance();
-
-const petServiceQueue = require('./lib/pet-service-queue');
-const companyName = chance.company();
-const queue = new petServiceQueue(companyName);
-
